757422aa91613360bd957c96950fac30f2673f9e
deva <devarangan2002@gmail.com>
2024-03-04 05:57:29 +0530

    home page implemented

--- src/app/page.js
+++ src/app/page.js
@@ -11,8 +11,8 @@ export default function Home() {
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
+            href="/home"
+            target="_self"
             rel="noopener noreferrer"
           >
             By{" "}
